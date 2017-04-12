892f0156ae8fd90c6a4ed22e95ec9a0f141bbf24
aaronrzh <287628769@qq.com>
2017-04-12 23:46:04 +0800

    first commit

---
+++ src/android/AutostartPlugin.java
@@ -0,0 +1,13 @@
+package com.aaronrzh.cordova.plugin.autostart;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.LOG;
+
+import android.content.Context;
+
+public class WhitelistPlugin extends CordovaPlugin {
+    private static final String LOG_TAG = "AutostartPlugin";
+
+    public AutostartPlugin() {
+    }
+}
---
+++ src/android/AutostartReceiver.java
@@ -0,0 +1,18 @@
+package com.aaronrzh.cordova.plugin.autostart;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AutostartReceiver extends BroadcastReceiver {
+    static final String ACTION = "android.intent.action.BOOT_COMPLETED";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(ACTION)) {
+            Intent mainActivityIntent = new Intent(context, MainActivity.class);
+            mainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(mainActivityIntent);
+        }
+    }
+}
